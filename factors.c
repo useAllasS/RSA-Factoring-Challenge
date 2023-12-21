93047b120a38b436412e3162824a9be7801220ce
Nita Youssef <nitayoussef3@gmail.com>
2023-12-21 01:23:59 -0800

    33

--- factors.c
+++ factors.c
@@ -2,23 +2,23 @@
 
 int main()
 {
-    long int num = 239809320265259;
-    long int i = 2;
-    long int k;
+	long int num = 239809320265259;
+	long int i = 2;
+	long int k;
 
-    while (num % i)
-    {
-        if (i <= num)
-        {
-            i++;
-        }
-        else
-        {
-            return (-1);
-        }
-    }
+	while (num % i)
+	{
+		if (i <= num)
+		{
+			i++;
+		}
+		else
+		{
+			return (-1);
+		}
+	}
 
-    k = num / i;
-    printf("%ld = %ld * %ld\n", num, k, i);
-    return (0);
+	k = num / i;
+	printf("%ld = %ld * %ld\n", num, k, i);
+	return (0);
 }
